44d71c5e1b5218d53692f95e60d5b349bcffa75d
Alexander Pisarev <70193123+APVerlan@users.noreply.github.com>
2021-03-19 18:59:32 +0300

    Update Генерация скобочных последовательностей.cpp

--- Пробный контест/Генерация скобочных последовательностей.cpp
+++ Пробный контест/Генерация скобочных последовательностей.cpp
@@ -6,9 +6,6 @@
 
 using namespace std;
 
-void SeqGenerator(string& base, size_t left, 
-	size_t right, bool flag, set<string>& data);
-
 void gen(int n, string& cur_seq, int cur_balance = 0, int cur_index = 0) {
 	if (cur_index == 2 * n) {
 		for (auto& bracket : cur_seq) {
@@ -53,45 +50,8 @@ int main() {
 	}
 
 	if (n) {
-		copy = first_seq;
-		seq.insert(first_seq);
-
-		/*SeqGenerator(first_seq, 0, first_seq.size(), false, seq);
-		SeqGenerator(copy, 0, copy.size(), true, seq);
-
-		for (auto& s : seq) {
-			cout << s << endl;
-		}*/
 		gen(n, first_seq);
-
 	}
 
 	return 0;
-}
-
-void SeqGenerator(string& base, size_t left, 
-	size_t right, bool flag, set<string>& data) {
-	if (right == 2)
-		return;
-	
-	if (left == right - 2)
-		return;
-	
-	if (!flag) {
-		size_t begin = (right - 1) / 2 + 1, end = right - 1;
-		for (size_t i = begin; i < end; ++i) {
-			swap(base[i], base[i - 1]);
-			data.insert(base);
-		}
-
-		SeqGenerator(base, left, right - 2, flag, data);
-	} else {
-		size_t begin = left + (right - 1 - left) / 2, end = left;
-		for (int i = begin; i > end; i--) {
-			swap(base[i], base[i + 1]);
-			data.insert(base);
-		}
-
-		SeqGenerator(base, left + 2, right, flag, data);
-	}
 }
